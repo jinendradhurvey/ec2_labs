8482c92e0e9804e412c30bebf79baef89f1d6039
jinendradhurvey <jinendra.dhurvey@cloudreach.com>
2022-07-22 16:33:45 +0530

    multiple instance

--- main.tf
+++ main.tf
@@ -1,18 +1,21 @@
-resource "aws_instance" "ansible_server" {
+resource "aws_instance" "ansible_servers" {
+  count = 5
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t3.micro"
+  instance_type = "t3.medium"
 
 vpc_security_group_ids = [aws_security_group.ansible_server.id]
 key_name = "talent-academy-lab"
 subnet_id = data.aws_subnet.talent_academy.id
- #EIP
+
   tags = {
-    Name = "ansible _server"
+    Name = "ansible _server {count.index + 1}$"
   }
 }
 
 resource "aws_eip" "ansible_server_ip" {
-  instance = aws_instance.ansible_server.id
+  count = 5
+
+  instance = aws_instance.ansible_servers[count.index].id
   vpc      = true
 
 }
